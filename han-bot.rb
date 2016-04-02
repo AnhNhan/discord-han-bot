ebb8dee51b4c2e38cfd50715aae67d272e7b892f
Anh Nhan Nguyen <anhnhan@outlook.com>
2016-04-02 02:36:48 +0200

    Support announcement of The Sheldt-ability

--- han-bot.rb
+++ han-bot.rb
@@ -61,6 +61,11 @@ module AnnouncePossibleGames
   extend Discordrb::EventContainer
 
   presence do |event|
+    online_users = event.server.users.select{ |u| u.status.equals?(:online) }
+    if online_users.length == 1
+      online_user_names = online_users.map(&:name).join
+      event.server.general_channel.send_message "There are a total of six people online. Perfect for a Company of Heroes match on The Sheldt! (#{online_user_names})"
+    end
   end
 end
 
