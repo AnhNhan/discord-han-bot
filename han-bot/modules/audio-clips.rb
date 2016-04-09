28d0d474935d6ce7709be966c254e3ca8ca32530
Anh Nhan Nguyen <anhnhan@outlook.com>
2016-04-09 20:56:08 +0200

    fix for audio controls

--- han-bot/modules/audio-clips.rb
+++ han-bot/modules/audio-clips.rb
@@ -58,17 +58,17 @@ module HanBot::Modules::AudioClips
   end
 
   message(content: "#audio-stop") do |event|
-    event.bot.voice(current_voice_channel(event.user, event.bot)).stop_playing
+    event.bot.voice(HanBot.current_voice_channel(event.user, event.bot)).stop_playing
     event.respond "Command received. Playback should stop within a few seconds."
   end
 
   message(content: "#audio-pause") do |event|
-    event.bot.voice(current_voice_channel(event.user, event.bot)).pause
+    event.bot.voice(HanBot.current_voice_channel(event.user, event.bot)).pause
     event.respond "Command received. Playback should pause within a few seconds."
   end
 
   message(content: "#audio-continue") do |event|
-    event.bot.voice(current_voice_channel(event.user, event.bot)).continue
+    event.bot.voice(HanBot.current_voice_channel(event.user, event.bot)).continue
     event.respond "Command received. Playback should continue within a few seconds."
   end
 
